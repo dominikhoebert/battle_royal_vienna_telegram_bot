06a4985071cb12db35f12824621545dd999fe7b3
dominikhoebert <42839893+dominikhoebert@users.noreply.github.com>
2024-06-18 15:28:02 +0200

    added bot_timer.py class

---
+++ bot_timer.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass, field
+from threading import Timer
+from time import time
+
+
+class BotTimer:
+    def __init__(self, name: str, interval: int, user_id: int, message: str = None, map: bool = False,
+                 config: int = None):
+        self.name = name
+        self.interval = interval
+        self.user_id = user_id
+        self.timer = Timer(interval * 60, timer_function, [self])
+        self.timer.start()
+        self.running = True
+        self.message = message
+        self.map = map
+        self.config = config
+        self.start_time = time()
+
+    def get_remaining_time(self):
+        return self.interval - ((time() - self.start_time) / 60)
+
+    def pause(self):
+        if self.running:
+            self.timer.cancel()
+            self.interval = self.get_remaining_time()
+            self.running = False
+
+    def resume(self):
+        if not self.running:
+            self.timer = Timer(self.interval * 60, timer_function, [self])
+            self.timer.start()
+            self.start_time = time()
+            self.running = True
+
+
+def timer_function(bot_timer):
+    print(f"Timer {bot_timer.name} finished")
+    ...
+
+
+if __name__ == "__main__":
+    t1 = BotTimer("test", 1, 1)
+    t2 = BotTimer("test2", 2, 1)
+    # t3 = BotTimer("test3", 3, 1)
+    # print(t1.get_remaining_time())
+    # print(t2.get_remaining_time())
+    # print(t3.get_remaining_time())
+    t1.pause()
+    t1.resume()
